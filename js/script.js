944eec01a110f2eeb109a47eaf2634cb31123098
Mikiyas <162327014+Mikiyas1221@users.noreply.github.com>
2025-05-09 18:25:22 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('nav-toggler').addEventListener('click', function () {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('active');
+});
